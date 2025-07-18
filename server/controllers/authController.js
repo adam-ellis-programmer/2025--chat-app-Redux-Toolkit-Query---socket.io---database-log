388d9f598b98dddd8dc67f2a0d2bd84b62909831
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-07-18 19:22:23 +0100

    main js file change

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -12,7 +12,7 @@ const setTokenCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true, // Prevents XSS attacks
     secure: isProduction, // HTTPS only in production
-    sameSite: 'strict', // CSRF protection
+    sameSite: 'lax', // CSRF protection
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
     path: '/', // Available for entire site
   })
