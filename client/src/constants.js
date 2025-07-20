3d433d4ea08505fc2edbced1a27e246cabac3685
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-07-20 11:12:30 +0100

    Updated env check constants.js file

--- client/src/constants.js
+++ client/src/constants.js
@@ -2,5 +2,7 @@ console.log('xxx')
 const isProduction = import.meta.env.VITE_NODE_ENV === 'production'
 console.log(isProduction)
 export const BASE_URL = isProduction
-  ? 'http://localhost:5001'
-  : 'https://socket-io-app-mern-deployed-production.up.railway.app'
+  ? 'https://socket-io-app-mern-deployed-production.up.railway.app'
+  : 'http://localhost:5001'
+
+// https://socket-io-app-mern-deployed-production.up.railway.app
